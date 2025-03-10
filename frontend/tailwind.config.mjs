0cc01cff23eba91f171c0e59e01edb4ce1c74014
Parames8803 <parameswaran8803@gmail.com>
2025-03-10 17:44:19 +0530

    changes made

--- frontend/tailwind.config.mjs
+++ frontend/tailwind.config.mjs
@@ -5,6 +5,9 @@ export default {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
   theme: {
     extend: {
       colors: {
